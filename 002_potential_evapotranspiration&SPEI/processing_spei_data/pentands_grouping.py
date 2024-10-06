9dc63574bd94e7856d5caf61c7f18f37ad67b386
lboestfleisch <luca_bost@yahoo.co.nz>
2024-10-06 21:57:38 +0200

    first try at summarizing the data into pentads

---
+++ 002_potential_evapotranspiration&SPEI/processing_spei_data/pentands_grouping.py
@@ -0,0 +1,84 @@
+"""
+File for grouping the days into pentads (5 days)
+working on the CMIP5 data 
+goal: to compare the CMIP5 data with the observational data for every 5 days 
+"""
+from netCDF4 import Dataset
+import os
+import numpy as np
+import netCDF4 as nc
+
+file_name = "spei_EUR-11_ICHEC-EC-EARTH_historical_r12i1p1_CLMcom-CCLM4-8-17_v1_day_1966-2005.nc"
+file_path = os.path.join("C:/03_Capstone/a_publishing/data/CMIP5_EUR-11_ICHEC-EC-EARTH_CLMcom-CCLM4-8-17/r12i1p1_v1/output", file_name)
+spei_data = Dataset(file_path, 'r')
+
+lon = spei_data.variables['rlon'][:]
+lat = spei_data.variables['rlat'][:]
+time = spei_data.variables['time'][:]
+
+"For the CMIP5 data"
+lat_min, lat_max = -6.0, 8.0
+lon_min, lon_max = -8.0, -1.0
+lon_length = 63
+lat_length = 128
+
+"have the first pentad starting on the 1970, so first need to crop the dataset of the past modelled data from starting point 1966 to 1970"
+start_day_CMIP5 = 4 * 365 + 1 #because it is 4 years in, and one leap year in the time frame
+end_day_CMIP5 = len(time)
+all_days = end_day_CMIP5 - start_day_CMIP5
+num_pentads = all_days // 5
+
+# maybe making list with the stored days and their corresponding pentad 
+
+Array_pentad = np.zeros((num_pentads, lon_length, lat_length), dtype=np.float32)  
+pentad_indices = np.zeros(all_days, dtype=np.int32)
+
+for j in range(lon_length):
+    for i in range(lat_length):
+        for p in range(num_pentads):
+            start_pentad = p * 5
+            end_pentad = start_pentad + 5
+
+            spei_var = spei_data.variables['spei'][start_pentad:end_pentad, j, i] #adapt
+            Array_pentad[p, j, i] = np.mean(spei_var)
+
+            pentad_indices[start_pentad:end_pentad] = p
+        print(f'lon: {j}, lat: {i}')
+
+
+output_directory = "C:/03_Capstone/a_publishing/data/CMIP5_EUR-11_ICHEC-EC-EARTH_CLMcom-CCLM4-8-17/r12i1p1_v1/output"
+output_file = "pentads_spei_EUR-11_ICHEC-EC-EARTH_historical_r12i1p1_CLMcom-CCLM4-8-17_v1_day_1966-2005.nc"
+
+with Dataset(os.path.join(output_directory, output_file), 'w', format='NETCDF4') as ds:
+    pentad = ds.createDimension('pentad', num_pentads)
+    lon = ds.createDimension('rlon', lon_length)  
+    lat = ds.createDimension('rlat', lat_length)  
+
+    pentads = ds.createVariable('pentads', 'f4', ('pentads',))
+    lons = ds.createVariable('rlon', 'f4', ('rlon',))  
+    lats = ds.createVariable('rlat', 'f4', ('rlat',)) 
+    pentad_avg = ds.createVariable('pentad_avg', 'f4', ('pentads', 'rlon', 'rlat'))  
+
+    pentad_avg.units = 'Unknown'
+
+    lons[:] = np.linspace(lon_min, lon_max, lon_length)
+    lats[:] = np.linspace(lat_min, lat_max,lat_length)
+    pentads[:] = np.arange(0, num_pentads, 1)
+
+    pentad_avg[:, :, :] = Array_pentad
+
+print("calculated the avg spei per pentad")
+
+ds_out = Dataset(os.path.join(output_directory, output_file), 'r')
+print("Dimensions in the output file:")
+for dim_name, dimension in ds_out.dimensions.items():
+    print(f"{dim_name}: {len(dimension)}")
+
+print("Dimensions in the input file:")
+for dim_name, dimension in spei_data.dimensions.items():
+    print(f"{dim_name}: {len(dimension)}")
+
+ds_out.close()
+spei_data.close()
+
+
